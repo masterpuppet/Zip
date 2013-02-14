af189c352c6a42a1a4c7c8c611ef86fef1d7757a
Abimael Rodríguez <abimaelrc@gmail.com>
2013-02-13 22:18:59 -0400

    Fix parentesis

--- Vendor/Zip/Extract.php
+++ Vendor/Zip/Extract.php
@@ -481,7 +481,7 @@ class Extract extends Zip
              */
             if (
                 in_array($fileInfo['name'], $this->getFilesToExtract()) === true
-                || $this->greedy === true && (in_array(basename($fileInfo['name']), $this->getFilesToExtract()) === true)
+                || ($this->greedy === true && in_array(basename($fileInfo['name']), $this->getFilesToExtract()) === true)
             ){
                 $files[] = $fileInfo['name'];
             }
